7453f5ce1156643816ee442eef027e04f0719bf0
Lucas Boesing Steffens <lucas.steffens2012@hotmail.com>
2023-07-19 19:52:14 -0300

    Animação no scroll

--- script.js
+++ script.js
@@ -60,3 +60,26 @@ function initScrollSuave() {
   });
 }
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  console.log(sections);
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+    console.log(windowMetade);
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        console.log(sectionTop);
+        if(sectionTop < 0) {
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimacaoScroll();
